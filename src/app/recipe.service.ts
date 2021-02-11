60a2881205a9d7637d570a9d61bae40a5823b090
Renjie Luo <lrj193927@gamil.com>
2021-02-11 18:48:53 -0500

    add ingredients to recipes

--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -1,13 +1,28 @@
 import {Recipe} from './recipes/recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from './recipes/shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg?resize=640%2C360&ssl=1'),
-    new Recipe('Fried Rice', 'This is Fried Rice', 'https://www.fifteenspatulas.com/wp-content/uploads/2012/03/Fried-Rice-Fifteen-Spatulas-8.jpg')
-  ];
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg?resize=640%2C360&ssl=1',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(
+      'Fried Rice',
+      'This is Fried Rice',
+      'https://www.fifteenspatulas.com/wp-content/uploads/2012/03/Fried-Rice-Fifteen-Spatulas-8.jpg',
+    [
+      new Ingredient('Buns', 2),
+      new Ingredient('Meat', 1)
+    ])
+];
 
   getRecipes(): Recipe[] {
     return this.recipes.slice();
